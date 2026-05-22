b752e858a55032e00a84b84ca84d97e1ccf75537
ranjit7500 <ran.singh778855@gmail.com>
2026-05-22 13:07:07 +0530

    Update 1_Grosery_Bill.py

--- 1_Grosery_Bill.py
+++ 1_Grosery_Bill.py
@@ -1,5 +1,7 @@
+# Taking purchase amount as input
 amount = float(input("Enter purchase amount: "))
 
+# Checking discount conditions
 if amount >= 5000:
     discount = amount * 0.20   # 20% discount
 elif amount >= 3000:
@@ -7,7 +9,8 @@ elif amount >= 3000:
 else:
     discount = amount * 0.05   # 5% discount
 
+# Calculating final amount
 final_amount = amount - discount
 
-
+# Displaying final bill
 print("Final Bill Amount =", final_amount)
