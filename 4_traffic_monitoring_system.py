4ad6397a65c0eebf9514a7ad3cdb218c5b9302bb
ranjit7500 <ran.singh778855@gmail.com>
2026-05-22 12:09:55 +0530

    Remove unnecessary line breaks in speed check

--- 4_traffic_monitoring_system.py
+++ 4_traffic_monitoring_system.py
@@ -1,10 +1,8 @@
-# Taking speed input
+
 speed = int(input("Enter vehicle speed: "))
 
-# Defining speed limit
 speed_limit = 60
 
-# Checking speed
 if speed > speed_limit:
     print("Vehicle exceeded the speed limit")
 else:
