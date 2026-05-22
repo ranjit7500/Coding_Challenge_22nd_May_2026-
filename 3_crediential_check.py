ca2339179a49501eb34edc60826ae58c859401b8
ranjit7500 <ran.singh778855@gmail.com>
2026-05-22 13:09:50 +0530

    Update 3_crediential_check.py

--- 3_crediential_check.py
+++ 3_crediential_check.py
@@ -1,10 +1,12 @@
+# Predefined credentials
 correct_username = "admin"
 correct_password = "1234"
 
+# Taking user input
 username = input("Enter username: ")
 password = input("Enter password: ")
 
-
+# Checking credentials
 if username == correct_username and password == correct_password:
     print("Login Successful")
 else:
