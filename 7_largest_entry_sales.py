a55a8cb34318835780c360edaa645f76f5c90373
ranjit7500 <ran.singh778855@gmail.com>
2026-05-22 12:13:49 +0530

    Add files via upload

---
+++ 7_largest_entry_sales.py
@@ -0,0 +1,11 @@
+
+sale1 = int(input("Enter first sales figure: "))
+sale2 = int(input("Enter second sales figure: "))
+sale3 = int(input("Enter third sales figure: "))
+
+if sale1 > sale2 and sale1 > sale3:
+    print("Largest Sales Figure =", sale1)
+elif sale2 > sale1 and sale2 > sale3:
+    print("Largest Sales Figure =", sale2)
+else:
+    print("Largest Sales Figure =", sale3)
