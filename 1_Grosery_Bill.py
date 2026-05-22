8119eff61acfe2693a95a88a36d97af2e576f069
ranjit7500 <ran.singh778855@gmail.com>
2026-05-22 11:34:50 +0530

    Add files via upload

---
+++ 1_Grosery_Bill.py
@@ -0,0 +1,15 @@
+
+amount = float(input("Enter purchase amount: "))
+
+if amount >= 5000:
+    discount = amount * 0.20   # 20% discount
+elif amount >= 3000:
+    discount = amount * 0.10   # 10% discount
+else:
+    discount = amount * 0.05   # 5% discount
+
+# Calculating final amount
+final_amount = amount - discount
+
+# Displaying final bill
+print("Final Bill Amount =", final_amount)
