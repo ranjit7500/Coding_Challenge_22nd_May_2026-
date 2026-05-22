68372c6d90c4c9900fb25bb19b8bca64ec9de477
ranjit7500 <ran.singh778855@gmail.com>
2026-05-22 13:13:19 +0530

    Update 7_largest_entry_sales.py

--- 7_largest_entry_sales.py
+++ 7_largest_entry_sales.py
@@ -1,7 +1,9 @@
+# Taking inputs
 sale1 = int(input("Enter first sales figure: "))
 sale2 = int(input("Enter second sales figure: "))
 sale3 = int(input("Enter third sales figure: "))
 
+# Finding largest number
 if sale1 > sale2 and sale1 > sale3:
     print("Largest Sales Figure =", sale1)
 elif sale2 > sale1 and sale2 > sale3:
