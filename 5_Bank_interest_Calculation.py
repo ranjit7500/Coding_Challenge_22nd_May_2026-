7647df2acb3491ac4b7631884a523530013b5864
ranjit7500 <ran.singh778855@gmail.com>
2026-05-22 12:12:58 +0530

    Add files via upload

--- 5_Bank_interest_Calculation.py
+++ 5_Bank_interest_Calculation.py
@@ -1,9 +1,10 @@
-
+# Taking inputs
 principal = float(input("Enter principal amount: "))
 rate = float(input("Enter rate of interest: "))
 time = float(input("Enter time in years: "))
 
-
+# Calculating simple interest
 si = (principal * rate * time) / 100
 
+# Displaying result
 print("Simple Interest =", si)
