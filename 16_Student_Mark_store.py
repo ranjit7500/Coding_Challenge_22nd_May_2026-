ec0f22680fea62a0ee17b6063e024214a631231a
ranjit7500 <ran.singh778855@gmail.com>
2026-05-22 13:17:04 +0530

    Update 16_Student_Mark_store.py

---
+++ 16_Student_Mark_store.py
@@ -0,0 +1,11 @@
+# List of marks
+marks = [75, 80, 90, 85, 70]
+
+# Calculate total marks
+total = sum(marks)
+
+# Calculate average
+average = total / len(marks)
+
+# Display average
+print("Average Marks:", average)
